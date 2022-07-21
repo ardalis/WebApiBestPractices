8e5afcb765a5a1e715b05bb1a143bbe0f48800b2
Steve Smith <steve@kentsmiths.com>
2022-07-20 20:02:46 -0400

    Refactoring tests

--- api_templates/APIProjectTests/Endpoints/GetById_Endpoint.cs
+++ api_templates/APIProjectTests/Endpoints/GetById_Endpoint.cs
@@ -9,6 +9,7 @@ using BackendData.DataAccess;
 using Microsoft.Extensions.Logging;
 using Xunit;
 using Xunit.Abstractions;
+using Ardalis.HttpClientTestExtensions;
 
 namespace APIProjectTests.Endpoints;
 
@@ -45,5 +46,52 @@ public class GetById_Endpoint
 
 		string expectedAuthorName = Authors.FirstOrDefault().Name;
 		Assert.Equal(expectedAuthorName, result.Name);
+	}
+
+	[Fact]
+	public async Task ReturnsSeededAuthorWithId1UsingLocalHelper()
+	{
+		// Arrange (done in WebApiApplication)
+
+		// Act
+		var result = await GetAndDeserialize(Routes.Authors.Get(Authors.FirstOrDefault().Id));
+
+		// Assert
+		Assert.NotNull(result);
+
+		string expectedAuthorName = Authors.FirstOrDefault().Name;
+		Assert.Equal(expectedAuthorName, result.Name);
+	}
+
+	private async Task<AuthorDto> GetAndDeserialize(string route)
+	{
+		var response = await _client.GetAsync(route);
+		response.EnsureSuccessStatusCode();
+		var stringResult = await response.Content.ReadAsStringAsync();
+		_logger.LogInformation(stringResult);
+		var result = JsonSerializer.Deserialize<AuthorDto>(stringResult,
+			JsonOptionConstants.SerializerOptions);
+		return result;
+	}
+
+	[Fact]
+	public async Task ReturnsNotFoundGivenInvalidId()
+	{
+		// Arrange
+		int invalidId = -1;
+
+		// Act
+		var response = await _client.GetAsync(Routes.Authors.Get(invalidId));
+
+		// Assert
+		Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+	}
+
+	[Fact]
+	public async Task ReturnsNotFoundGivenInvalidIdWithHelper()
+	{
+		int invalidId = -1;
+
+		await _client.GetAndEnsureNotFound(Routes.Authors.Get(invalidId), _testOutputHelper);
 	}
 }
